31d53e4b563e109ff196d74cbc5fa66410385d40
Patrick Cauley <patrick@cranesandcaffeine.com>
2015-03-06 19:38:35 -0500

    Removed shrinkwrap installed 0.13 compat react router and some other useful dependencies

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,15 +1,11 @@
-const React = require('react');
+const React      = require('react');
 const Superagent = require('superagent');
-const ContextHelper = require('../helpers/ContextHelper');
 
-/**
- * Main React application entry-point for both the server and client.
- *
- * @module Main
- */
+
 const Main = React.createClass({
   render() {
     return <div>
+      <h1>Hello World.</h1>
       <h1>Hello World.</h1>
     </div>;
   }
